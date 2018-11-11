1de4ba48365df568c527a98acd48b6f83e5ce995
sripwoud <me@sripwoud.xyz>
2018-11-11 10:36:38 +0100

    fix: Fix relationships. Add cascade delete

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,3 @@
-import sys
 from sqlalchemy import (
     Column, ForeignKey, Integer, String, create_engine
 )
@@ -7,30 +6,41 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+
 class User(Base):
+    """Define app's User class."""
+
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
     email = Column(String(80), nullable=False)
-    picture = Column(String(300))
 
     @property
     def serialize(self):
+        """Return object data in serializeable format."""
         return {
             'name': self.name,
-            'id': self.id
-            'email': self.email
+            'id': self.id,
+            'email': self.email,
             'picture': self.picture
         }
 
 
 class Genre(Base):
+    """Genre class."""
+
     __tablename__ = "genre"
     name = Column(String(20), nullable=False)
     id = Column(Integer, primary_key=True)
 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    album = relationship("Album", cascade="all, delete-orphan")
+    song = relationship("Song", cascade="all, delete-orphan")
+
     @property
     def serialize(self):
+        """Return object data in serializeable format."""
         return {
             'name': self.name,
             'id': self.id
@@ -38,6 +48,8 @@ class Genre(Base):
 
 
 class Album(Base):
+    """Album (release) class."""
+
     __tablename__ = "album"
     id = Column(Integer, primary_key=True)
     title = Column(String(80), nullable=False)
@@ -48,9 +60,13 @@ class Album(Base):
 
     genre_id = Column(Integer, ForeignKey('genre.id'))
     genre = relationship(Genre)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    song = relationship("Song", cascade="all, delete-orphan")
 
     @property
     def serialize(self):
+        """Return object data in serializeable format."""
         return {
             'id': self.id,
             'title': self.title,
@@ -62,16 +78,23 @@ class Album(Base):
 
 
 class Song(Base):
+    """Song class."""
+
     __tablename__ = "song"
     id = Column(Integer, primary_key=True)
     position = Column(String(4), nullable=False)
     title = Column(String(80), nullable=False)
 
+    genre_id = Column(Integer, ForeignKey('genre.id'))
+    genre = relationship(Genre)
     release_id = Column(Integer, ForeignKey('album.id'))
     album = relationship(Album)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
+        """Return object data in serializeable format."""
         return {
             'id': self.id,
             'position': self.position,
